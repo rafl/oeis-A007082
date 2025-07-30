310f1c11ec2f5dad8b52e98b197efba328b62683
Florian Ragwitz <florian.ragwitz@gmail.com>
2025-07-30 06:30:07 -0700

    Snapshot every 30 minutes only

--- src/snapshot.c
+++ src/snapshot.c
@@ -56,7 +56,7 @@ static void *snapshot(void *ud) {
   while (!st->quit) {
     struct timespec now;
     clock_gettime(_CLOCK, &now);
-    now.tv_sec += 5;
+    now.tv_sec += 60*30;
     pthread_cond_timedwait(&st->cv, &st->mu, &now);
     if (st->quit) break;
 
