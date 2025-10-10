ccdd3c67816a12e03fd832fa5103837a525fbc8c
Florian Ragwitz <florian.ragwitz@gmail.com>
2025-10-09 23:41:42 -0700

    Basic comment support for source_combine input

--- src/source_combine.c
+++ src/source_combine.c
@@ -1,12 +1,33 @@
 #include "source_combine.h"
 #include "debug.h"
 
+#include <ctype.h>
 #include <inttypes.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 static int stdin_next(source_t *, uint64_t *res, uint64_t *p) {
-  return scanf("%" SCNu64 " %% %" SCNu64, res, p) == 2;
+  while (1) {
+    char *l;
+    size_t cap = 0;
+    ssize_t n = getline(&l, &cap, stdin);
+    if (n < 0) {
+      free(l);
+      return n;
+    }
+    char *s = l;
+    while (*s && isspace((unsigned char)*s))
+      s++;
+    if (*s == '\0' || *s == '\n' || *s == '#') {
+      free(l);
+      continue;
+    }
+
+    bool ok = sscanf(s, "%" SCNu64 " %% %" SCNu64, res, p) == 2;
+    free(l);
+    return ok;
+  }
 }
 
 static void stdin_destroy(source_t *src) { free(src); }
